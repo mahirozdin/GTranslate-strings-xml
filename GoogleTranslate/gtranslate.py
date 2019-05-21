5bf8945121ef95272ed8a5492775c6aa2a8190ff
Mahir Taha Özdin <ozdinmt@gmail.com>
2019-05-21 22:27:07 +0300

    File creating , Translation Improvements
    File creating structure changed.  Italian and French language translations fixed.

--- GoogleTranslate/gtranslate.py
+++ GoogleTranslate/gtranslate.py
@@ -18,28 +18,31 @@ INPUTLANGUAGE = input()
 print("Enter the output language code tr etc.")
 OUTPUTLANGUAGE  = input()
 
-print ("Enter the in filename strings.xml etc.")
-INFILE = input()
-print("Enter the out filename strings_new_translated.xml etc.")
-OUTFILE = input()
+path = "values-"+OUTPUTLANGUAGE;
+INFILE = "strings.xml"
+OUTFILE = path+"/strings.xml"
+
+
+try:  
+    os.mkdir(path)
+except OSError:  
+    print ("Creation of the directory %s failed" % path)
+else:  
+    print ("Successfully created the directory %s " % path)
 
 def translate(to_translate, to_language="auto", language="auto"):
  r = requests.get("http://translate.google.com/m?hl=%s&sl=%s&q=%s"% (to_language, language, to_translate.replace(" ", "+")))
  beforecharset='charset='
  aftercharset='" http-equiv'
  parsed1=r.text[r.text.find(beforecharset)+len(beforecharset):]
- parsed2=parsed1[:parsed1.find(aftercharset)]
 
+ r.encoding='cp874'
  
- if(parsed2!=r.encoding):
-     print('\x1b[1;31;40m' + 'Warning: Potential Charset conflict' )
-     print(" Encoding as extracted by SELF    : "+parsed2)
-     print(" Encoding as detected by REQUESTS : "+r.encoding+ '\x1b[0m')
-
 
  if(r.encoding=='windows-874' and os.name=='posix'):
      print('\x1b[1;31;40m' + "Alert: Working around age old Python bug (https://bugs.python.org/issue854511)\nOn Linux, charset windows-874 must be labeled as charset cp874"+'\x1b[0m')
      r.encoding='cp874'
+
 
 
  text=html.unescape(r.text)    
@@ -48,7 +51,7 @@ def translate(to_translate, to_language="auto", language="auto"):
  parsed1=r.text[r.text.find(before_trans)+len(before_trans):]
  parsed2=parsed1[:parsed1.find(after_trans)]
  print(parsed2) 
- return html.unescape(parsed2)
+ return html.unescape(parsed2).replace("'", r"\'")
  
 
 tree = ET.parse(INFILE)
@@ -73,7 +76,7 @@ for i in range(len(root)):
             if(isTranslatable=='false'):
                 print("Not translatable")
             if(root[i][j].tag=='item') & (isTranslatable!='false'):
-                totranslate=root[i][j].text
+               
                 print(totranslate)
                 if(totranslate!=None):
                     root[i][j].text=translate(totranslate,OUTPUTLANGUAGE,INPUTLANGUAGE)
