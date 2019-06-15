62b32b74b8304e2e70366a06301f399eb1fc97c8
Mahir Taha Özdin <ozdinmt@gmail.com>
2019-06-16 00:50:16 +0300

    Multi Translation on start.
    Now you can select multi languages to start translation.

--- GoogleTranslate/gtranslate.py
+++ GoogleTranslate/gtranslate.py
@@ -4,7 +4,7 @@
 
 ### Settings 
 
-
+# -*- coding: utf-8 -*-
 
 import html
 import requests
@@ -18,69 +18,67 @@ INPUTLANGUAGE = input()
 print("Enter the output language code tr etc.")
 OUTPUTLANGUAGE  = input()
 
-path = "values-"+OUTPUTLANGUAGE;
-INFILE = "strings.xml"
-OUTFILE = path+"/strings.xml"
+words = OUTPUTLANGUAGE.split(",")
+for current_word in words:
+	path = "values-"+current_word;
+	INFILE = "strings.xml"
+	OUTFILE = path+"/strings.xml"
+	
+	
+	try:  
+		os.mkdir(path)
+	except OSError:  
+		print ("Creation of the directory %s failed" % path)
+	else:  
+		print ("Successfully created the directory %s " % path)
+	
+	def translate(to_translate, to_language="auto", language="auto"):
+	 r = requests.get("http://translate.google.com/m?hl=%s&sl=%s&q=%s"% (to_language, language, to_translate.replace(" ", "+")))
+	 beforecharset='charset='
+	 aftercharset='" http-equiv'
+	 parsed1=r.text[r.text.find(beforecharset)+len(beforecharset):]
+
+	
 
 
-try:  
-    os.mkdir(path)
-except OSError:  
-    print ("Creation of the directory %s failed" % path)
-else:  
-    print ("Successfully created the directory %s " % path)
 
-def translate(to_translate, to_language="auto", language="auto"):
- r = requests.get("http://translate.google.com/m?hl=%s&sl=%s&q=%s"% (to_language, language, to_translate.replace(" ", "+")))
- beforecharset='charset='
- aftercharset='" http-equiv'
- parsed1=r.text[r.text.find(beforecharset)+len(beforecharset):]
-
- r.encoding='cp874'
+	 text=html.unescape(r.text)    
+	 before_trans = 'class="t0">'
+	 after_trans='</div><form'
+	 parsed1=r.text[r.text.find(before_trans)+len(before_trans):]
+	 parsed2=parsed1[:parsed1.find(after_trans)]
+	 print(parsed2) 
+	 return html.unescape(parsed2).replace("'", r"\'")
  
 
- if(r.encoding=='windows-874' and os.name=='posix'):
-     print('\x1b[1;31;40m' + "Alert: Working around age old Python bug (https://bugs.python.org/issue854511)\nOn Linux, charset windows-874 must be labeled as charset cp874"+'\x1b[0m')
-     r.encoding='cp874'
-
-
-
- text=html.unescape(r.text)    
- before_trans = 'class="t0">'
- after_trans='</div><form'
- parsed1=r.text[r.text.find(before_trans)+len(before_trans):]
- parsed2=parsed1[:parsed1.find(after_trans)]
- print(parsed2) 
- return html.unescape(parsed2).replace("'", r"\'")
- 
-
-tree = ET.parse(INFILE)
-root = tree.getroot()
-for i in range(len(root)):
-    isTranslatable=root[i].get('translatable')
-    print((str(i)+" ========================="))
-    if(isTranslatable=='false'):
-        print("Not translatable")
-    if(root[i].tag=='string') & (isTranslatable!='false'):
-        totranslate=root[i].text
-        print(totranslate)
-        print("-->")
-        if(totranslate!=None):
-            root[i].text=translate(totranslate,OUTPUTLANGUAGE,INPUTLANGUAGE)
-            print(root[i].text)
-    if(root[i].tag=='string-array'):
-        print("Entering string array...")
-        for j in range(len(root[i])):
-            isTranslatable=root[i][j].get('translatable')
-            print((str(i)+" "+str(j)+" ========================="))
-            if(isTranslatable=='false'):
-                print("Not translatable")
-            if(root[i][j].tag=='item') & (isTranslatable!='false'):
-               
-                print(totranslate)
-                if(totranslate!=None):
-                    root[i][j].text=translate(totranslate,OUTPUTLANGUAGE,INPUTLANGUAGE)
-                    print(root[i][j].text)
+	tree = ET.parse(INFILE)
+	root = tree.getroot()
+	for i in range(len(root)):
+		isTranslatable=root[i].get('translatable')
+		print((str(i)+" ========================="))
+		if(isTranslatable=='false'):
+			print("Not translatable")
+		if(root[i].tag=='string') & (isTranslatable!='false'):
+			totranslate=root[i].text
+			print(totranslate)
+			print("-->")
+			if(totranslate!=None):
+				print(current_word)
+				root[i].text=translate(totranslate,current_word,INPUTLANGUAGE)
+				print(root[i].text)
+		if(root[i].tag=='string-array'):
+			print("Entering string array...")
+			for j in range(len(root[i])):
+				isTranslatable=root[i][j].get('translatable')
+				print((str(i)+" "+str(j)+" ========================="))
+				if(isTranslatable=='false'):
+					print("Not translatable")
+				if(root[i][j].tag=='item') & (isTranslatable!='false'):
+				
+					print(totranslate)
+					if(totranslate!=None):
+						root[i][j].text=translate(totranslate,current_word,INPUTLANGUAGE)
+						print(root[i][j].text)
      
-tree.write(OUTFILE, encoding='utf-8')
+	tree.write(OUTFILE, encoding='utf-8')
 
